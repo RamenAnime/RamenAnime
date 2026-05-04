b0280cc57e58f06ba61eb5de0ba8e6ff66eb7bb3
RamenAnime <jasonjones21@att.net>
2026-05-04 15:56:56 -0400

    Fix drizzle SSL for TiDB Cloud

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,11 +6,17 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is required to run drizzle commands");
 }
 
+// Parse URL to add SSL for TiDB Cloud if not present
+const url = new URL(connectionString);
+if (!url.searchParams.has("ssl")) {
+  url.searchParams.set("ssl", "true");
+}
+
 export default defineConfig({
   schema: "./db/schema.ts",
   out: "./db/migrations",
   dialect: "mysql",
   dbCredentials: {
-    url: connectionString,
+    url: url.toString(),
   },
 });
